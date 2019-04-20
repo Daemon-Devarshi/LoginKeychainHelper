95d79e626c3dd9a0b4e29f19a71456ba14e65926
Devarshi <devarshi>
2019-04-21 04:37:09 +0530

    Initial commit.

---
+++ LoginKeychainHelper/LoginKeychainHelper.h
@@ -0,0 +1,16 @@
+//
+//  LoginKeychainHelper.h
+//  LoginKeychainHelper
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoginKeychainHelper.
+FOUNDATION_EXPORT double LoginKeychainHelperVersionNumber;
+
+//! Project version string for LoginKeychainHelper.
+FOUNDATION_EXPORT const unsigned char LoginKeychainHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoginKeychainHelper/PublicHeader.h>
+
+
---
+++ LoginKeychainHelper/SecureCredentials.swift
@@ -0,0 +1,41 @@
+//
+//  SecureCredentials.swift
+//  ACEApp
+//
+
+import Foundation
+
+public struct SecureCredentials : Codable {
+    public var userId: String
+    public var userName: String
+    public var userPassword: String
+    public var firebaseUID: String
+    
+    public func data() throws -> Data{
+        do {
+            let encoder = JSONEncoder()
+            encoder.outputFormatting = .prettyPrinted
+            return try encoder.encode(self)
+        }
+        catch {
+            throw error
+        }
+    }
+    
+    public init(userId: String, userName: String, userPassword: String, firebaseUID: String) {
+        self.userId = userId
+        self.userPassword = userPassword
+        self.userName = userName
+        self.firebaseUID = firebaseUID
+    }
+    
+    public init(data: Data) throws {
+        do {
+            let decoder = JSONDecoder()
+            self = try decoder.decode(SecureCredentials.self, from: data)
+        }
+        catch {
+            throw error
+        }
+    }
+}
